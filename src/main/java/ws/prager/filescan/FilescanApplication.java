5004a23c7af93c746de00e33d0ec7e18cfd719e3
Bernd Prager <bernd@prager.ws>
2016-06-15 13:07:13 -0400

    initial commit

---
+++ src/main/java/ws/prager/filescan/FilescanApplication.java
@@ -0,0 +1,12 @@
+package ws.prager.filescan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilescanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilescanApplication.class, args);
+	}
+}
